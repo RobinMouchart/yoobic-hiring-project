e0486d817775fa89dcf18700b595a81c425ff396
Robin Mouchart <Robin.Mouchart@gmail.com>
2019-12-19 21:21:04 +0100

    Added SignIn Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Sign Out',
+      url: '/sign-in',
+      icon: 'log-out'
     }
   ];
 
